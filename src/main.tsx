36b4f06641384dc8a1b025b0e95a91dbf7b04193
om2010ome-afk <om2010ome@gmail.com>
2026-06-06 15:46:23 +0300

    📦 Upload: src/main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,22 @@
+import { StrictMode, useEffect } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { useStore } from './store/useStore';
+
+function Root() {
+  const initAuth = useStore((s) => s.initAuth);
+
+  useEffect(() => {
+    const unsubscribe = initAuth();
+    return unsubscribe;
+  }, [initAuth]);
+
+  return <App />;
+}
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Root />
+  </StrictMode>
+);
